2a42c72b22ac0ec162720e6a92d03aae2c4af876
Limna <1026198339@qq.com>
2020-09-27 17:45:02 +0800

    完成图形报表

--- src/router/index.js
+++ src/router/index.js
@@ -13,6 +13,7 @@ const Params = () => import('../components/goods/Params.vue')
 const List = () => import('../components/goods/List.vue')
 const Add = () => import('../components/goods/Add.vue')
 const Order = () => import('../components/order/Order.vue')
+const Report = () => import('../components/report/Report.vue')
 
 Vue.use(Router)
 
@@ -38,7 +39,8 @@ const routes = [
       { path: '/params', component: Params },
       { path: '/goods', component: List },
       { path: '/goods/add', component: Add },
-      { path: '/orders', component: Order }
+      { path: '/orders', component: Order },
+      { path: '/reports', component: Report }
     ]
   }
 ]
